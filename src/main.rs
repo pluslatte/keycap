3dfb260770863a12e587285053b6836d6e54a480
pluslatte <debrisfield0201@gmail.com>
2024-12-16 22:48:27 +0900

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("hello");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let html_str = "<!DOCTYPE html>
+    <html>
+        <head>
+            <title>skibidi</title>
+        </head>
+        <body>
+            <h1>Skibidi</h1>
+        </body>
+    </html>";
+    let test = warp::path("hoge").map(|| warp::reply::html(html_str.to_string()));
+
+    warp::serve(test).run(([127, 0, 0, 1], 3030)).await;
 }

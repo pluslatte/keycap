acd9383bb3c1fc3db48981d169574996c1203bd6
pluslatte <debrisfield0201@gmail.com>
2024-12-17 00:22:59 +0900

    post test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
+use warp::Filter;
+
 #[tokio::main]
 async fn main() {
     // HEY! keep in mind that warp::path("hoge").and(warp::fs::dir("somewhere/something")) WON'T WORK!
     let front = warp::fs::dir("front/build");
-    warp::serve(front).run(([127, 0, 0, 1], 3030)).await;
+
+    let note = warp::post().map(|| {
+        println!("POST");
+        "ok"
+    });
+    warp::serve(warp::any().and(front.or(note)))
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }

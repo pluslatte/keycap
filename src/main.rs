ebe8e6d8dfbc77de673375d90c4d40d7c593b6d9
pluslatte <debrisfield0201@gmail.com>
2024-12-17 00:11:46 +0900

    read this comment

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[tokio::main]
 async fn main() {
-    warp::serve(warp::fs::dir("front/build"))
-        .run(([127, 0, 0, 1], 3030))
-        .await;
+    // HEY! keep in mind that warp::path("hoge").and(warp::fs::dir("somewhere/something")) WON'T WORK!
+    let front = warp::fs::dir("front/build");
+    warp::serve(front).run(([127, 0, 0, 1], 3030)).await;
 }

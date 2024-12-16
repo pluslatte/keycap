bb2fb46450d1024b1383f36043234be1f0eeab9a
pluslatte <debrisfield0201@gmail.com>
2024-12-16 20:55:10 +0900

    keep

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Ok;
+use misskey::prelude::*;
+use misskey::HttpClient;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let client = HttpClient::builder("https://virtualkemomimi.net/api/")
+        .token("WzVTz8JZKRwD1H5iErKr1W10HeWfdsPV")
+        .build()?;
+
+    if let Err(e) = client.create_note("test").await {
+        println!("{:?}", e);
+    }
+
+    Ok(())
 }

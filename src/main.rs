169f26f1558e973e848a4bae8e3c1d2f72c1a0c8
pluslatte <debrisfield0201@gmail.com>
2024-12-16 21:27:43 +0900

    that's enough I'll do it myself

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,3 @@
-use anyhow::Ok;
-use misskey::prelude::*;
-use misskey::HttpClient;
-
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let client = HttpClient::builder("https://virtualkemomimi.net/api/")
-        .token("WzVTz8JZKRwD1H5iErKr1W10HeWfdsPV")
-        .build()?;
-
-    if let Err(e) = client.create_note("つかれた").await {
-        println!("{:?}", e);
-    }
-
-    Ok(())
+fn main() {
+    println!("hello");
 }

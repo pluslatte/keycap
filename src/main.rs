c080c704bd0493876e2f83efaf493f99dc5cf587
pluslatte <debrisfield0201@gmail.com>
2024-12-16 23:41:02 +0900

    wow

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
-use warp::Filter;
-
 #[tokio::main]
 async fn main() {
-    let html_str = "<!DOCTYPE html>
-    <html>
-        <head>
-            <title>skibidi</title>
-        </head>
-        <body>
-            <h1>Skibidi</h1>
-        </body>
-    </html>";
-    let test = warp::path("hoge").map(|| warp::reply::html(html_str.to_string()));
-
-    warp::serve(test).run(([127, 0, 0, 1], 3030)).await;
+    warp::serve(warp::fs::dir("front/build"))
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }

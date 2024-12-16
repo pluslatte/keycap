9ee37851e51885ef85ae624e4da1fbb6fda2591d
pluslatte <debrisfield0201@gmail.com>
2024-12-17 02:24:55 +0900

    note text from input field

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-const onNoteButtonClicked = () => {
-  fetch("http://localhost:3030", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      text: "テスト"
-    }),
-  }).then((response) => {
-    if (!response.ok) {
-      console.error(response);
-      throw new Error("status is not 200");
-    }
-  });
-};
-
 function App() {
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const onInputFieldChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setInputValue(value);
+  }
+
+  const onNoteButtonClicked = () => {
+    fetch("http://localhost:3030", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        text: inputValue
+      }),
+    }).then((response) => {
+      if (!response.ok) {
+        console.error(response);
+        throw new Error("status is not 200");
+      }
+    });
+  };
+
   return (
     <div className="App">
+      <div>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={onInputFieldChange}
+          placeholder="Type something..."
+        />
+      </div>
       <button onClick={onNoteButtonClicked}>
         ノート
       </button>

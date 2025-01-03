e3d80ed908cbf522b2bf71ae8a731328d167dcb2
pluslatte <debrisfield0201@gmail.com>
2025-01-04 02:20:53 +0900

    Update keycap_server.nix to improve build inputs and add postFixup step for client files

--- keycap_server.nix
+++ keycap_server.nix
@@ -10,8 +10,13 @@ rustPlatform.buildRustPackage {
   pname = "keycap";
   version = "0.1.0";
 
-  buildInputs = [ openssl ];
-  nativeBuildInputs = [ pkg-config (pkgs.callPackage ./keycap_front.nix { }) ];
+  buildInputs = [ openssl (pkgs.callPackage ./keycap_front.nix { }) ];
+  nativeBuildInputs = [ pkg-config ];
+
+  postFixup = ''
+    mkdir -p $out/www
+    cp -r www/keycap-client $out/www
+  '';
 
   src = ./.;
   cargoLock.lockFile = ./Cargo.lock;

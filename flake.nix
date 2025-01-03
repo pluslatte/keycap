ae562b2ca405fceb7260b7fd4684aca013b99e58
pluslatte <debrisfield0201@gmail.com>
2025-01-03 10:22:09 +0900

    build server with nix flakes

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { nixpkgs
+    , flake-utils
+    , rust-overlay
+    , ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            openssl
+            pkg-config
+            rust-bin.stable.latest.default
+          ];
+        };
+
+        packages.default = pkgs.callPackage ./keycap_server.nix { };
+      }
+    );
+}
---
+++ keycap_server.nix
@@ -0,0 +1,18 @@
+{ makeRustPlatform, rust-bin, openssl, pkg-config, }:
+let
+  toolchain = rust-bin.stable.latest.default;
+  rustPlatform = makeRustPlatform {
+    cargo = toolchain;
+    rustc = toolchain;
+  };
+in
+rustPlatform.buildRustPackage {
+  pname = "keycap-server";
+  version = "0.1.0";
+
+  buildInputs = [ openssl ];
+  nativeBuildInputs = [ pkg-config ];
+
+  src = ./.;
+  cargoLock.lockFile = ./Cargo.lock;
+}

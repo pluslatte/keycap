4f21d38be1511fc2d91f2a939b478d96313dab64
pluslatte <debrisfield0201@gmail.com>
2025-01-04 09:12:08 +0900

    Update keycap_server.nix to include keycap_client in buildInputs and enhance preInstall process

--- keycap_server.nix
+++ keycap_server.nix
@@ -5,19 +5,53 @@ let
     cargo = toolchain;
     rustc = toolchain;
   };
+  keycapClient = pkgs.callPackage ./keycap_client.nix { };
 in
 rustPlatform.buildRustPackage
 {
   pname = "keycap";
   version = "0.1.0";
 
-  buildInputs = [ openssl (pkgs.callPackage ./keycap_client.nix { }) ];
+  buildInputs = [ openssl keycapClient ];
   nativeBuildInputs = [ pkg-config ];
 
   src = ./.;
   cargoLock.lockFile = ./Cargo.lock;
 
-  postFixup = ''
-    cp -r keycap-client $out/
+  # postBuild = ''
+  #   echo "postBuild"
+  #   ls -la keycap-client
+  # '';
+  # postInstall = ''
+  #   echo "postInstall"
+  #   ls -la keycap-client
+  # '';
+  # postFixup = ''
+  #   echo "postFixup"
+  #   ls -la keycap-client
+  # '';
+  # postConfigure = ''
+  #   echo "postConfigure"
+  #   ls -la keycap-client
+  # '';
+
+  # preBuild = ''
+  #   echo "preBuild"
+  #   ls -la keycap-client
+  # '';
+  preInstall = ''
+    echo "preInstall"
+    ls -la keycap-client
+    ls -la ${keycapClient.outPath}
+    mkdir -p $out
+    cp -r ${keycapClient.outPath}/keycap-client $out/
   '';
+  # preFixup = ''
+  #   echo "preFixup"
+  #   ls -la keycap-client
+  # '';
+  # preConfigure = ''
+  #   echo "preConfigure"
+  #   ls -la keycap-client
+  # '';
 }
